674705eb5c5d5b531634ac7b0fd3cc58acb1a9d7
Bigbits <bigbits@hackx.cc>
2019-07-22 14:15:49 +0800

    Add arduino framework.

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -1,1 +1,73 @@
-# TODO
+from os.path import isdir, join
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+
+board = env.BoardConfig()
+
+FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-arduino-gd32v")
+assert FRAMEWORK_DIR and isdir(FRAMEWORK_DIR)
+SDK_DIR = join(FRAMEWORK_DIR, "cores", "arduino", "GD32VF103_Firmware_Library")
+
+env.SConscript("_bare.py", exports="env")
+
+env.Append(
+
+    CPPPATH = [
+        join(FRAMEWORK_DIR, "cores", "arduino"),
+        join(FRAMEWORK_DIR, "cores", "arduino", "deprecated-avr-comp"),
+        join(SDK_DIR, "GD32VF103_standard_peripheral"),
+        join(SDK_DIR, "GD32VF103_standard_peripheral", "Include"),
+        join(SDK_DIR, "GD32VF103_usbfs_driver"),
+        join(SDK_DIR, "GD32VF103_usbfs_driver", "Include"),
+        join(SDK_DIR, "n22", "drivers"),
+        join(SDK_DIR, "n22", "env_Eclipse"),
+        join(SDK_DIR, "n22", "stubs"),
+        join(SDK_DIR, "Utilities"),
+        join(SDK_DIR, "Utilities", "LCD_common"),
+    ],
+
+    LIBS = [
+        "c"
+    ],
+
+    LIBSOURCE_DIRS=[
+        join(FRAMEWORK_DIR, "libraries")
+    ],
+
+)
+
+env.Replace(
+    LDSCRIPT_PATH = join(SDK_DIR, "n22", "env_Eclipse", board.get("build.ldscript")) 
+)
+
+#
+# Target: Build Core Library
+#
+
+libs = []
+
+if "build.variant" in env.BoardConfig():
+    env.Append(
+        CPPPATH=[
+            join(FRAMEWORK_DIR, "variants",
+                 env.BoardConfig().get("build.variant"))
+        ]
+    )
+    libs.append(env.BuildLibrary(
+        join("$BUILD_DIR", "FrameworkArduinoVariant"),
+        join(FRAMEWORK_DIR, "variants", env.BoardConfig().get("build.variant"))
+    ))
+
+envsafe = env.Clone()
+
+libs.append(envsafe.BuildLibrary(
+    join("$BUILD_DIR", "FrameworkArduino"),
+    join(FRAMEWORK_DIR, "cores", "arduino")
+))
+
+
+
+env.Prepend(LIBS=libs)
+
---
+++ examples/arduino-blink/src/Blink.cpp
@@ -0,0 +1,28 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include <Arduino.h>
+
+// Set LED_BUILTIN if it is not defined by Arduino framework
+// #define LED_BUILTIN 2
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  delay(1000);
+}
